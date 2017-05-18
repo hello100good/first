6376389f8c5d0dd2109885bd69484e67b6a64c10
hello100good <470760633@qq.com>
2017-05-18 13:53:27 +0800

    Add files via upload

---
+++ moduleInsertAjax.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="moduleInsertAjax.aspx.cs" Inherits="moduleInsertAjax" %>
+
+
---
+++ moduleInsertAjax.aspx.cs
@@ -0,0 +1,31 @@
+﻿
+    using DBUtility;
+    using System;
+    using System.Web.UI;
+
+public partial class moduleInsertAjax : Page
+    {
+        private BaseClass bc = new BaseClass();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string lname = base.Server.UrlDecode(base.Request.QueryString["lname"].ToString());
+                string lRemark = base.Server.UrlDecode(base.Request.QueryString["lRemark"].ToString());
+                string lVersion = base.Server.UrlDecode(base.Request.QueryString["lVersion"].ToString());
+                string strnum = "l" + DateTime.Now.ToString("yyMMddHHmmss");
+                string sqladd = "insert into module(lNum,lname,lRemark,lStates)";
+                string name3 = sqladd;
+                sqladd = name3 + "values('" + strnum + "','" + lname + "','" + lRemark + "','" + lVersion + "')";
+                this.bc.execsql(sqladd);
+                base.Response.Write(strnum);
+            }
+            catch
+            {
+                base.Response.Write("false");
+            }
+        }
+    }
+
+
